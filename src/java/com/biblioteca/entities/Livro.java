4d27054a85d3420785ddc6e0c0677c010c948017
Murilo Xavier <muquinhaxl@gmail.com>
2017-12-11 11:52:42 -0200

    Implementação dos métodos CRUD da Livro.

--- src/java/com/biblioteca/entities/Livro.java
+++ src/java/com/biblioteca/entities/Livro.java
@@ -1,48 +1,145 @@
 package com.biblioteca.entities;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+import com.biblioteca.db.Database;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Livro {
+    private int id;
     private String titulo;
     private String tipo;
     private String descricao;
     private String isbn;
 
-    public Livro(String titulo, String tipo, String descricao, String isbn) {
+    public Livro(int id, String titulo, String tipo, String descricao, String isbn) {
+        this.id = id;
         this.titulo = titulo;
         this.tipo = tipo;
         this.descricao = descricao;
         this.isbn = isbn;
     }
-
+    // Método responsável pela adição de livros a db.
+    public void setLivro(String titulo, String tipo, String descricao, String isbn){
+        try{
+           String SQL = "INSERT INTO livro VALUES (default, ?, ?, ?, ?)";
+           PreparedStatement statement = Database.getConnection().prepareStatement(SQL);
+           statement.setString(1, titulo);
+           statement.setString(2, tipo);
+           statement.setString(3, descricao);
+           statement.setString(4, isbn);
+           statement.execute();
+           statement.close();
+        }catch(Exception exception){
+            System.out.println("[class:Livro][catch:setLivro]: "+exception.getMessage());
+        }
+    }
+    
+    // Método que retorna um livro pelo id (retorna objeto Livro ou null).
+    public Livro getLivro(int id){
+        Livro livro = null;
+        try{
+            String SQL = "SELECT * FROM livro WHERE cd_livro = "+id+"";
+            Statement statement = Database.getConnection().createStatement();
+            ResultSet resultSet = statement.executeQuery(SQL);
+            if(resultSet.next()){
+                livro = new Livro(resultSet.getInt("cd_livro"), 
+                                  resultSet.getString("nm_livro"),
+                                  resultSet.getString("nm_tipo_livro"), 
+                                  resultSet.getString("ds_livro"), 
+                                  resultSet.getString("cd_isbn_livro"));
+            }
+            statement.close();
+        }catch(Exception exception){
+            System.out.println("[class:Livro][catch:getLivro]: "+exception.getMessage());
+        }
+        return livro;
+    }
+    
+    // Este método efetua uma busca pelos livros com base em um parametro.
+    // Retorna um ArrayList de livros encontrados com(retorna Arraylist<Livros> ou null).
+    public List<Livro> getSearchedLivro(String context){
+        ArrayList<Livro> livros = null;
+        try{
+            String SQL = "SELECT * FROM livro WHERE nm_livro LIKE '%?%'";
+            PreparedStatement statement = Database.getConnection().prepareStatement(SQL);
+            statement.setString(1, context);
+            ResultSet resultSet = statement.executeQuery();
+            while(resultSet.next()){
+                Livro livro = new Livro(resultSet.getInt("cd_livro"), 
+                                  resultSet.getString("nm_livro"),
+                                  resultSet.getString("nm_tipo_livro"), 
+                                  resultSet.getString("ds_livro"), 
+                                  resultSet.getString("cd_isbn_livro"));
+                livros.add(livro);
+            }
+            statement.close();
+        }catch(Exception exception){
+            System.out.println("[class:Livro][catch:getSearchedLivro]: "+exception.getMessage());
+        }
+        return livros;
+    }
+    
+    // Método responsável por atualizar todos os itens de um livro por seu id.
+    // HINT: Utilize um formulário que já tenha todos os valores setados nele (value) e
+    // permita ao usuário alterar somente o que quer.
+    public void updateLivro(int id, String titulo, String tipo, String descricao, String isbn){
+        try{
+            String SQL = "UPDATE livro SET "
+                    + "nm_livro = ?"
+                    + ", nm_tipo_livro = ?"
+                    + ", ds_livro = ?"
+                    + ", cd_isbn_livro = ?"
+                    + "WHERE cd_livro = ?";
+            PreparedStatement statement = Database.getConnection().prepareStatement(SQL);
+            statement.setString(1, titulo);
+            statement.setString(2, tipo);
+            statement.setString(3, descricao);
+            statement.setString(4, isbn);
+            statement.setInt(5, id);
+            statement.execute();
+            statement.close();
+        }catch(Exception exception){
+            System.out.println("[class:Livro][catch:updateLivro]: "+exception.getMessage());
+        }
+    }
+    
+    // Método que retorna um ArrayList<Livro> de livros.
+    public List<Livro> getLivros(){
+        ArrayList<Livro> livros = new ArrayList<>();
+        try{
+            Statement statement = Database.getConnection().createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM livro");
+            while(resultSet.next()){
+                Livro livro = new Livro(resultSet.getInt("cd_livro"), 
+                                  resultSet.getString("nm_livro"),
+                                  resultSet.getString("nm_tipo_livro"), 
+                                  resultSet.getString("ds_livro"), 
+                                  resultSet.getString("cd_isbn_livro"));
+                livros.add(livro);
+            }
+            statement.close();
+        }catch(Exception exception){
+            System.out.println("[class:Livro][catch:getLivros]: "+exception.getMessage());
+        }
+        return livros;
+    }
+    
     public String getTitulo() {
         return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
     }
 
     public String getTipo() {
         return tipo;
     }
 
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
     public String getDescricao() {
         return descricao;
     }
 
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
     public String getIsbn() {
         return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-    
+    }    
 }
